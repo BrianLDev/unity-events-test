787cf6dbdc2ab4d867967cb20121b8d35cde12d5
Brian Leip <brianleip@gmail.com>
2020-07-14 09:57:56 -0700

    Updated to 2019.4.3f1, renamed scenes, changed script only scene to separate Static and Non-Static

--- Assets/Scripts/01/EventSubscriber_NonStatic.cs
+++ Assets/Scripts/01/EventSubscriber_NonStatic.cs
@@ -3,14 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EventSubscriber : MonoBehaviour {
+public class EventSubscriber_NonStatic : MonoBehaviour {
     // SUBSCRIBER CLASS
 
-    EventPublisher publisher;  // need a reference to the publisher class to subscribe to events.  If the publisher class is static, this is not needed and can reference the class directly
+    public EventPublisher_NonStatic publisher;  // need a reference to the publisher class to subscribe to events.  If the publisher class is static, this is not needed and can reference the class directly
+                                                // publisher should be assigned in the Unity editor, but the below code in OnEnable() checks if it was assigned properly and Finds it if not
+
+    private void OnEnable() {
+        if (!publisher)
+            publisher = GameObject.Find("Publisher (Non-Static)").GetComponent<EventPublisher_NonStatic>();
+        if (!publisher)
+            Debug.LogError("Error: Publisher Script not found.  Cannot run Events Test Demo.");
+    }
     
-        
     private void Start() {
-        publisher = GetComponent<EventPublisher>();
         publisher.OnEPressed += OtherClassFunction;    // OtherClassFunction is now subscribed to OnEPressed
         publisher.OnEPressed += UnsubscribeFunction;   // UnsubscribeTest is now subscribed to OnEPressed
         publisher.OnReturnPressed += OtherClassFunction;    // OtherClassFunction is now subscribed to OnEnterPressed
@@ -23,7 +29,7 @@ public class EventSubscriber : MonoBehaviour {
     public void OtherClassFunction(object sender, EventArgs e) {
         Debug.Log("OtherClassFunction triggered by " + sender);
     }
-    public void OtherClassFunction(object sender, EventPublisher.OnReturnPressedEventArgs e) {
+    public void OtherClassFunction(object sender, EventPublisher_NonStatic.OnReturnPressedEventArgs e) {
         // alternate version that takes the custom event args for OnReturnPressed
         Debug.Log("OtherClassFunction triggered by " + sender + ".  Return count = " + e.returnPressedCount);
     }
@@ -32,7 +38,7 @@ public class EventSubscriber : MonoBehaviour {
             ".  Now unsubscribing this func from OnSpace and should not come up again.");
         publisher.OnEPressed -= UnsubscribeFunction;
     }
-    public void UnsubscribeFunction(object sender, EventPublisher.OnReturnPressedEventArgs e) {
+    public void UnsubscribeFunction(object sender, EventPublisher_NonStatic.OnReturnPressedEventArgs e) {
         // alternate version that takes the custom event args for OnReturnPressed
         Debug.Log("UnsubscribeFunction triggered by " + sender +
             ".  Now unsubscribing this func from OnReturn and should not come up again.");
@@ -44,7 +50,7 @@ public class EventSubscriber : MonoBehaviour {
     public void ActionFunction() {
         Debug.Log("ActionFunction triggered.");
     }
-    public void ActionFunction(bool b, int i, EventPublisher.OnReturnPressedEventArgs e) {
+    public void ActionFunction(bool b, int i, EventPublisher_NonStatic.OnReturnPressedEventArgs e) {
         Debug.Log("ActionFunction triggered. Bool = " + b + ", Int = " + i + ", Return count = " + e.returnPressedCount);
     }
 }
